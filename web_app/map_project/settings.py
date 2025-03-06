dcad630c66fa314bcf9dc8257c22e8f36a8febcf
Tae wook <laputa99999@gmail.com>
2025-03-06 17:58:31 +0900

    Add files via upload

--- web_app/map_project/settings.py
+++ web_app/map_project/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-rffl57l_sm_7dx!zo=+g39q^iymvn#6&^=h0+l8gu^*8477a#9'
+SECRET_KEY = '<input SECRET_KEY>'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
